9eb0835021d65d55859698987b3f62d7f1676d5f
Karl <westerholm.karl@gmail.com>
2023-02-08 20:44:44 -0500

    updated to grid layout, added new function and buttons, changed action buttons to symbols

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
 
+  function newNum(e,n) {
+    e.preventDefault();
+    inputRef.current.value = inputRef.current.value * 10 + n;
+  }
+  
   function plus(e) {
     e.preventDefault();
     setResult((result) => result + Number(inputRef.current.value));
@@ -54,12 +59,24 @@ function App() {
               type="number"
               placeholder="Type a number"
           />
-          <button onClick={plus}>add</button>
-          <button onClick={minus}>subtract</button>
-          <button onClick={times}>multiply</button>
-          <button onClick={divide}>divide</button>
-          <button onClick={resetInput}>reset input</button>
-          <button onClick={resetResult}>reset result</button>
+          <div id="buttons">
+            <button onClick={plus}>+</button>
+            <button onClick={minus}>-</button>
+            <button onClick={times}>*</button>
+            <button onClick={divide}>/</button>
+            <button onClick={(e) => newNum(e,1)}>1</button>
+            <button onClick={(e) => newNum(e,2)}>2</button>
+            <button onClick={(e) => newNum(e,3)}>3</button>
+            <button onClick={(e) => newNum(e,4)}>4</button>
+            <button onClick={(e) => newNum(e,5)}>5</button>
+            <button onClick={(e) => newNum(e,6)}>6</button>
+            <button onClick={(e) => newNum(e,7)}>7</button>
+            <button onClick={(e) => newNum(e,8)}>8</button>
+            <button onClick={(e) => newNum(e,9)}>9</button>
+            <button onClick={(e) => newNum(e,0)}>0</button>
+            <button onClick={resetInput}>reset input</button>
+            <button onClick={resetResult}>reset result</button>
+          </div>
         </form>
       </div>
   );
